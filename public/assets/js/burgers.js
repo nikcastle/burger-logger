425d9da50e62b042809ff814c4015b363a196b27
Lady Stock <nikki.castlea@gmail.com>
2020-08-01 15:16:43 -0600

    added function to delete burgers

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,8 +1,7 @@
+$(function () {
 
-$(function() {
-    
     //function to change devour status
-    $(".change-devour").on("click", function(event) {
+    $(".change-devour").on("click", function (event) {
         const id = $(this).data("id");
         const newDevour = $(this).data("newdevour");
 
@@ -15,7 +14,7 @@ $(function() {
             type: "PUT",
             data: newDevourState
         }).then(
-            function(){
+            function () {
                 console.log("Changed devour to ", newDevour);
                 location.reload();
             }
@@ -24,7 +23,7 @@ $(function() {
     })
 
     //create burger form & function
-    $(".create-form").on("click", function(event) {
+    $(".create-form").on("click", function (event) {
         event.preventDefault();
 
         const newBurger = {
@@ -36,20 +35,28 @@ $(function() {
         $.ajax("/api/burgers/", {
             type: "POST",
             data: newBurger
-        }).then( function() {
+        }).then(function () {
             console.log("Added new burger");
             location.reload();
         });
 
     });
 
-    
-    //TODO: build delete burger function
 
+    //delete burger function
+    $(".delete-burger").on("click", function (event) {
+        const id = $(this).data("id");
 
+        //Send ajax delete call
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("Deleted Burger #" + id);
+                location.reload();
+            }
+        );
 
-
-
-
+    });
 
 })
