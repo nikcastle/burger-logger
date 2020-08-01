2d475fa1af8920377bc929413f3e381392675906
Lady Stock <nikki.castlea@gmail.com>
2020-08-01 15:11:51 -0600

    added burger names to seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 INSERT INTO burgers (name)
 VALUES 
-("Imppossible Cheeseburger"),
-("Bleu Cheese Burger"),
-("Guacamole Burger"),
-("Salmon Burger");
+("Plymouth Roquefort Burger"),
+("We're Here, We're Gruyere, Get Used to it Burger"),
+("Put Me in Poached Burger"),
+("Poutine on the Ritz Burger");
 
 INSERT INTO burgers (name, devoured)
 VALUES 
-("Cheeseburger"),
-("Maple Bacon Burger"),
+("I Don't Bay Leaf in Magic Burger"),
+("She's a Super Leek Burger"),
 ("Running Out of Thyme Burger");
