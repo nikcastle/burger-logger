b078cbd716accc2832806f3a2ce800616855f4a1
Lady Stock <nikki.castlea@gmail.com>
2020-07-31 18:44:17 -0600

    setting up schema and seed files

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (name)
+VALUES 
+("Imppossible Cheeseburger"),
+("Bleu Cheese Burger"),
+("Guacamole Burger"),
+("Salmon Burger");
+
+INSERT INTO burgers (name, devoured)
+VALUES 
+("Cheeseburger"),
+("Maple Bacon Burger"),
+("Running Out of Thyme Burger");
