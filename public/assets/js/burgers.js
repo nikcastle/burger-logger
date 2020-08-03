d0813f634f67a56b29bee4fd89110e11eb92bdc0
Lady Stock <nikki.castlea@gmail.com>
2020-08-03 17:59:32 -0600

    updated image files

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -5,31 +5,29 @@ $(function () {
         const id = $(this).data("id");
         const newDevour = $(this).data("newdevour");
 
-        const newDevourState = {
-            devoured: newDevour
-        }
-
         //Send the put request, use ajax call
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
-            data: newDevourState
+           
         }).then(
             function () {
                 console.log("Changed devour to ", newDevour);
                 location.reload();
             }
         )
-
+        console.log({newDevour});
+        console.log({id});
     })
 
     //create burger form & function
-    $(".create-form").on("click", function (event) {
+    $(".create-form").on("submit", function (event) {
         event.preventDefault();
-
+        
         const newBurger = {
             name: $("#burg").val().trim(),
-            // devoured: $("[name=devoured]:checked").val().trim();
         };
+
+        if (newBurger.name === "")return;
 
         //Send the Post request
         $.ajax("/api/burgers/", {
